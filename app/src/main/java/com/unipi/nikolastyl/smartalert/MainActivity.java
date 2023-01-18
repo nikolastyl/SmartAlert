c32e775774cb2d86356a937471e4a1f4a25f49fc
nikolastyl <nikostyliyis@gmail.com>
2023-01-18 12:46:53 +0200

    Initial commit

---
+++ app/src/main/java/com/unipi/nikolastyl/smartalert/MainActivity.java
@@ -0,0 +1,14 @@
+package com.unipi.nikolastyl.smartalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
